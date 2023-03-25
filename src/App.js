73616b0677aa27328ee79b77827ce8cca5914b22
John Hysong <99369137+johnatticus@users.noreply.github.com>
2023-03-25 14:22:10 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Configuration, OpenAIApi } from 'openai';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import FormSection from './components/FormSection';
+import AnswerSection from './components/AnswerSection';
+
+import { useState } from 'react';
+
+const App = () => {
+	const configuration = new Configuration({
+		apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+	});
+
+	const openai = new OpenAIApi(configuration);
+
+	const [storedValues, setStoredValues] = useState([]);
+
+	const generateResponse = async (newQuestion, setNewQuestion) => {
+		let options = {
+			model: 'text-davinci-003',
+			temperature: 0,
+			max_tokens: 100,
+			top_p: 1,
+			frequency_penalty: 0.0,
+			presence_penalty: 0.0,
+			stop: ['/'],
+		};
+
+		let completeOptions = {
+			...options,
+			prompt: newQuestion,
+		};
+
+		const response = await openai.createCompletion(completeOptions);
+
+		if (response.data.choices) {
+			setStoredValues([
+				{
+					question: newQuestion,
+					answer: response.data.choices[0].text,
+				},
+				...storedValues,
+			]);
+			setNewQuestion('');
+		}
+	};
+
+	return (
+		<div>
+			<div className="header-section">
+				<h1>ChatGPT CLONE 🤖</h1>
+				{storedValues.length < 1 && (
+					<p>
+						I am an automated question and answer system, designed to assist you
+						in finding relevant information. You are welcome to ask me any
+						queries you may have, and I will do my utmost to offer you a
+						reliable response. Kindly keep in mind that I am a machine and
+						operate solely based on programmed algorithms.
+					</p>
+				)}
+			</div>
+
+			<FormSection generateResponse={generateResponse} />
+
+			{storedValues.length > 0 && <AnswerSection storedValues={storedValues} />}
+		</div>
+	);
+};
 
 export default App;
